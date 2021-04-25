894221d9b4a9567a7e36013f68da8605c5ce2903
Renata <renatacristovams@gmail.com>
2021-04-24 23:58:28 -0300

    Aditional Nubank

---
+++ Nubank/Nubank/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Nubank
+//
+//  Created by RENATA on 21/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ Nubank/Nubank/edges.swift
@@ -0,0 +1,41 @@
+//
+//  edges.swift
+//  Nubank
+//
+//  Created by RENATA on 24/04/21.
+//
+
+import Foundation
+
+import UIKit
+
+@IBDesignable extension UIButton {
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+    }
+}
